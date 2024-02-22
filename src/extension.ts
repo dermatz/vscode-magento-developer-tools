e5301d0412309bd0b7cbf32303f463f40e08c57a
Mathias Elle <m.elle@dermatz.de>
2024-02-22 20:21:56 +0100

    remove empty spaces

--- src/extension.ts
+++ src/extension.ts
@@ -3,9 +3,7 @@ import { createStatusBarItem } from './status/statusbar';
 
 export function activate(context: vscode.ExtensionContext) {
 
-
-
-	createStatusBarItem();
+    createStatusBarItem();
 
 	let disposable = vscode.commands.registerCommand('magento-developer-tools.start', () => {
 		vscode.window.showInformationMessage('Magento Developer Tools is started!', { modal: false });
