2606a0e281dfe66e133f2064f45d723a24300302
Mathias Elle <m.elle@dermatz.de>
2024-02-23 09:11:13 +0100

    Update package.json and add detectMagentoRoot command

--- src/extension.ts
+++ src/extension.ts
@@ -1,15 +1,17 @@
 import * as vscode from 'vscode';
+import { detectMagento } from './magento/detect';
 import { createStatusBarItem } from './status/statusbar';
 
-export function activate(context: vscode.ExtensionContext) {
+export async function activate(context: vscode.ExtensionContext) {
+    if ((await detectMagento()) === true) {
+        createStatusBarItem();
+    }
 
-    createStatusBarItem();
+    let disposable = vscode.commands.registerCommand('magento-developer-tools.start', () => {
+        vscode.window.showInformationMessage('Magento Developer Tools!');
+    });
 
-	let disposable = vscode.commands.registerCommand('magento-developer-tools.start', () => {
-		vscode.window.showInformationMessage('Magento Developer Tools!');
-	});
-
-	context.subscriptions.push(disposable);
+    context.subscriptions.push(disposable);
 }
 
 export function deactivate() {}
---
+++ src/magento/detect.ts
@@ -0,0 +1,31 @@
+import * as vscode from 'vscode';
+import config from '../config/config';
+
+/**
+ * Registers the `magento-developer-tools.detectMagentoRoot` command.
+ */
+vscode.commands.registerCommand('magento-developer-tools.detectMagentoRoot', detectMagento);
+
+/**
+ * Detects the Magento root directory by searching for the `app/bootstrap.php` file.
+ * If the `magentoRoot` configuration is provided, it will search for the file in that directory.
+ * If the file is found, it displays an information message and returns `true`.
+ * If the file is not found, it displays an error message and returns `false`.
+ *
+ * @returns A boolean indicating whether the Magento root directory was found or not.
+ */
+export async function detectMagento() {
+    let magentoRoot = config.get('magentoRoot');
+    if (magentoRoot && (await vscode.workspace.findFiles(`${magentoRoot}/app/bootstrap.php`)).length > 0) {
+        vscode.window.showInformationMessage(`Magento Developer Tools: Magento root found in ${magentoRoot}`);
+        return true;
+    } else if (!magentoRoot && (await vscode.workspace.findFiles(`app/bootstrap.php`)).length > 0) {
+        vscode.window.showInformationMessage('Magento Developer Tools: Magento root found');
+        return true;
+    } else {
+        vscode.window.showErrorMessage('Magento Developer Tools: Magento root not found');
+        return false;
+    }
+}
+
+export default detectMagento;
