69884bb14ef2323a684d9106df38507dd46bbf9f
Mathias Elle <m.elle@dermatz.de>
2024-02-24 22:07:24 +0100

    Update placeholder text for theme location selection

--- src/magento/copyCommand.ts
+++ src/magento/copyCommand.ts
@@ -5,7 +5,7 @@ export function copyCommand(context: vscode.ExtensionContext) {
         let magentoThemeLocations = vscode.workspace.getConfiguration('magento-developer-tools').get('magentoThemeLocations');
         if (magentoThemeLocations) {
             let location = await vscode.window.showQuickPick(magentoThemeLocations, {
-                placeHolder: 'Select a theme location to copy to',
+                placeHolder: 'Select a Magento location to copy to',
             });
             if (location) {
 
